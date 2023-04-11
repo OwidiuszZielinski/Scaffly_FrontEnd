833c163a5f0df13290aa79470f953f00597810f3
Owidiusz Zieliński <owi19955@gmail.com>
2023-04-11 13:50:12 +0200

    start project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main style="height: 100%;background: url('Background2.jpg') no-repeat center center" justify="center" >
+  <v-app id="inspire">
+    
+
+    <v-app-bar
+    
+    app
+    color="#83817D">
+    <v-col md="5"></v-col>
+    <v-toolbar-title class="text-center" style="font-family: 'Consolas'; font-size: 40px; font-weight: bold">
+       Scaffly
+      </v-toolbar-title>
+
+          <v-col></v-col>
+          <v-col md="1">
+              <v-btn elevation=0 link v-on:click="route('/login')" color="#DBA02C">
+                <v-icon>mdi-account</v-icon>
+                <v-col >
+                  LOGIN
+                </v-col>
+              </v-btn>
+            </v-col>
+            <v-col md="1" >
+              <v-btn elevation=0 link v-on:click="route('/register')" color="#DBA02C">
+                <v-icon>mdi-account-plus</v-icon>
+                <v-col >
+                  REGISTER
+                </v-col>
+              </v-btn>
+            </v-col>
+          
+
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+  </main>
 </template>
+<style>
+</style>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+ 
+    data: () => ({ drawer: null }),
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+  }
+  
+</script>
