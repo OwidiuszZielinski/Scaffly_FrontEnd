9bfb429dbaae88ff0d3ea1bcd4aff5b8e5e20228
Owidiusz Zieliński <blackcloud@MacBook-Air-Elzbieta.local>
2023-04-11 15:36:24 +0200

    add typing header

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-
+import VueTyperPlugin from 'vue-typer'
+Vue.use(VueTyperPlugin)
 Vue.use(Vuetify);
 
 export default new Vuetify({
